0730345268b1f0455190b657b2d1cac73d16510f
wangyanfeng <yanfengking@163.com>
2021-01-17 17:07:54 +0800

    v1.0.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vcoolwind/go_demo
+
+go 1.15
+
+require github.com/vcoolwind/go_greet v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vcoolwind/go_greet"
+)
+
+func main(){
+	fmt.Println(go_greet.Greet("GoModule"))
+}
+
