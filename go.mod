0d602ce75e678aab1fc958b2656152fce85b0f8f
wangyanfeng <yanfengking@163.com>
2021-01-17 19:09:08 +0800

    add doc

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/vcoolwind/go_demo
 
 go 1.15
 
-require github.com/vcoolwind/go_greet v1.0.1
+require github.com/vcoolwind/go_greet v1.1.2
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+// go module study
+// 1 - https://segmentfault.com/a/1190000018398763
+// 2 - https://segmentfault.com/a/1190000018414744
+// 3- https://segmentfault.com/a/1190000019222071
 import (
 	"fmt"
 	"github.com/vcoolwind/go_greet"
 )
 
-func main(){
+func main() {
 	fmt.Println(go_greet.Greet("GoModule"))
 }
-
